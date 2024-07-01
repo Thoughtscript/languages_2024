a98b846e2e32b6e96443897127839c0c2abc6645
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-07-01 11:23:54 -0500

    tweaks

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# rm -rf kotlin_2024 
-# rm -rf python_processing_2024
-# rm -rf rrr_2024
+rm -rf kotlin_2024 
+rm -rf python_processing_2024
+rm -rf rrr_2024
+rm -rf node/async_error_node_2024
 
-# git clone https://github.com/Thoughtscript/kotlin_2024.git &
-# git clone https://github.com/Thoughtscript/python_processing_2024.git &
-# git clone https://github.com/Thoughtscript/rrr_2024.git &
+git clone https://github.com/Thoughtscript/kotlin_2024.git &
+git clone https://github.com/Thoughtscript/python_processing_2024.git &
+git clone https://github.com/Thoughtscript/rrr_2024.git &
+cd node && git clone https://github.com/Thoughtscript/async_error_node_2024.git &
 
 sleep 30 && docker-compose up
 
