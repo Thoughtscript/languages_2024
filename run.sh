e717f35f022ff9aa76d8b30d4e62b4e376027e09
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-11-20 13:19:26 -0600

    php

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ rm -rf python_processing_2024
 rm -rf rrr_2024
 rm -rf node/async_error_node_2024
 rm -rf java/java_algos
+rm -rf php_2024
 
 git clone https://github.com/Thoughtscript/kotlin_2024.git &
 git clone https://github.com/Thoughtscript/python_processing_2024.git &
 git clone https://github.com/Thoughtscript/rrr_2024.git &
+git clone https://github.com/Thoughtscript/php_2024.git &
 cd java && git clone https://github.com/Thoughtscript/java_algos.git &
 cd node && git clone https://github.com/Thoughtscript/async_error_node_2024.git &
 
